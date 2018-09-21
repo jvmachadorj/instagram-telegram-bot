69015d67d84b99719bdb9f404e5eb1a8aeb10cc8
JoaoMachado <jvmachadorj@gmail.com>
2018-09-21 15:42:57 -0300

    Add feature to not display if a picture was already posted

--- upload_photos.py
+++ upload_photos.py
@@ -1,56 +1,75 @@
 import ast
 import datetime
 import os
+import time
 from random import randint
 import requests
+from decouple import config
 
 from models import Image
 
+TAGS = ['travel', 'nature']
+CATEGORY = ['nature', 'backgrounds']
+URL = "https://pixabay.com/api/"
+KEY = config('PIXA_BAY_KEY')
+per_page = "100"
+
 
 def upload_photos():
-    tags = ['travel', 'nature']
-    category = ['nature', 'backgrounds']
-    url = "https://pixabay.com/api/"
-    key = "8943266-b5890d7d36c17ce3d56bf0676"
-    per_page = "50"
+    global per_page
+    query_tags = "+".join(TAGS)
+    query_categories = ",".join(CATEGORY)
 
-    query_tags = "+".join(tags)
-    query_categories = ",".join(category)
-
-    querystring = {"key": key, "q": query_tags, "image_type": "photo", "category": query_categories,
+    querystring = {"key": KEY, "q": query_tags, "image_type": "photo",
+                   "category": query_categories,
                    "safesearch": "true", "per_page": per_page}
 
-    response = requests.request("GET", url, params=querystring)
+    response = requests.request("GET", URL, params=querystring)
 
     result = ast.literal_eval(response.text)['hits']
     return result
 
 
 def download_photo():
-
-    n = randint(0, 50)
-
+    global per_page
+    global status
+    n = randint(0, int(per_page) - 1)
     result = upload_photos()[n]
+    image_obj = Image.get_or_none(pixabay_id=result['id'])
+    error = True
 
-    if Image.get_or_none(pixabay_id=result['id']) is None:
-        try:
-            url = result["largeImageURL"]
-            print(url)
-            filename = url.split('/')[-1]
-            script_path = os.path.dirname(__file__)
-            rel_path = 'images/{}'.format(filename)
-            path = os.path.join(script_path, rel_path)
-            r = requests.get(url, allow_redirects=True)
-            open(path, 'wb').write(r.content)
-            image = save_to_db(result, path, url)
-            return image
+    while error:
+        if image_obj is None:
+            try:
+                url = result["largeImageURL"]
+                print(url)
+                filename = url.split('/')[-1]
+                script_path = os.path.dirname(__file__)
+                rel_path = 'images/{}'.format(filename)
+                path = os.path.join(script_path, rel_path)
+                r = requests.get(url, allow_redirects=True)
+                open(path, 'wb').write(r.content)
+                image = save_to_db(result, path, url)
+                return image
+            except IOError as e:
+                return e
 
-        except IOError as e:
-            return e
-    else:
-        print("Image already downloaded and in database")
-        image = Image.get(pixabay_id=result['id'])
-        return image
+        if image_obj.status == "Posted":
+            i = 0
+            status = True
+            while i <= int(per_page) and status:
+                n = randint(0, int(per_page) - 1)
+                time.sleep(5)
+                result = upload_photos()[n]
+                image_obj = Image.get_or_none(pixabay_id=result['id'])
+                if image_obj.status == "Not Posted":
+                    print("Image already downloaded and in database")
+                    image = Image.get(pixabay_id=result['id'])
+                    return image
+                else:
+                    i += 1
+                    status = True
+        per_page = int(per_page)+10
 
 
 def treat_tags(tags):
@@ -60,7 +79,7 @@ def treat_tags(tags):
 def create_random_caption(tags):
     #TODO Tratar as tags e retornar como #
     treated_tags = treat_tags(tags)
-    return "Que bela de um comentario {}".format(treated_tags)
+    return "Que belo de um comentario {}".format(treated_tags)
 
 
 def save_to_db(line, path, url):
@@ -74,7 +93,14 @@ def save_to_db(line, path, url):
     tags = line['tags']
     caption = create_random_caption(tags)
 
-    image = Image.create(name=name, url=url, path=path, pixabay_id=pixabay_id, created_at=created_at, status=status,
+    image = Image.create(name=name, url=url, path=path, pixabay_id=pixabay_id,
+                         created_at=created_at, status=status,
                          tags=tags, caption=caption)
     image.save()
     return image
+
+
+def change_image_status(image):
+    image.status = "Posted"
+    image.save()
+
